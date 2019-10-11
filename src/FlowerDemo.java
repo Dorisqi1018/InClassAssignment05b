c6aec46834257b6557478da27d98a6e701dfbc57
Dorisqi1018 <wq2143@tc.columbia.edu>
2019-10-11 17:43:21 -0400

    Initial commit

---
+++ src/FlowerDemo.java
@@ -0,0 +1,27 @@
+public class FlowerDemo {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Flower f1 = new Flower(30, 30);
+        f1.draw();
+
+        Flower f2 = new Flower(550, 70);
+        f2.draw();
+
+        Flower f3 = new Flower(1010, 90);
+        f3.draw();
+        Flower f4 = new Flower(1030, 50);
+        f4.draw();
+        Flower f5 = new Flower(620, 10);
+        f5.draw();
+        Flower f6 = new Flower(130, 90);
+        f6.draw();
+        Flower f7 = new Flower(670, 90);
+        f7.draw();
+        Flower f8 = new Flower(590, 40);
+        f8.draw();
+
+
+    }
+}
